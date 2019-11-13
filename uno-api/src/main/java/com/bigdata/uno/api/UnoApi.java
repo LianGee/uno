3c1420900ebe05dd1a1eb73eb9921bbeedac36bf
zaoshu <zaoshu@pinduoduo.com>
2019-11-13 21:27:27 +0800

    升级spring 版本

--- uno-api/src/main/java/com/bigdata/uno/api/UnoApi.java
+++ uno-api/src/main/java/com/bigdata/uno/api/UnoApi.java
@@ -4,38 +4,16 @@ import com.bigdata.uno.service.ServiceContext;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.ImportResource;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import javax.servlet.ServletContextEvent;
-import javax.servlet.ServletContextListener;
 
 @Slf4j
 @SpringBootApplication
 @ImportResource({ "classpath*:appcontext-database.xml"})
 @Import(value = {ServiceContext.class})
 @EnableSwagger2
-public class UnoApi {
-
-    @Bean
-	protected ServletContextListener listener() {
-		return new ServletContextListener() {
-
-			@Override
-			public void contextInitialized(ServletContextEvent sce) {
-				log.info("ServletContext initialized");
-			}
-
-			@Override
-			public void contextDestroyed(ServletContextEvent sce) {
-				log.info("ServletContext destroyed");
-			}
-
-		};
-	}
-
+public class UnoApi {ll
 
     public static void main(String[] args) {
         SpringApplication.run(UnoApi.class, args);
