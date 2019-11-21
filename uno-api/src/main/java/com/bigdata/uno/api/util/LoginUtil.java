38d9fbefa6f5a8c971e62d4c1785e97cb94f5aa1
zaoshu <zaoshu@pinduoduo.com>
2019-11-21 13:40:17 +0800

    sso

--- uno-api/src/main/java/com/bigdata/uno/api/util/LoginUtil.java
+++ uno-api/src/main/java/com/bigdata/uno/api/util/LoginUtil.java
@@ -1,7 +1,5 @@
 package com.bigdata.uno.api.util;
 
-import com.alibaba.fastjson.JSON;
-import com.bigdata.uno.common.model.user.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -13,8 +11,6 @@ public class LoginUtil {
     private HttpServletRequest request;
 
     public String getLoginUser() {
-        String userString = request.isRequestedSessionIdValid() ? (String) request.getSession().getAttribute("user") : null;
-        User user = JSON.parseObject(userString, User.class);
-        return user == null ? null : user.getName();
+        return request.getUserPrincipal().getName();
     }
 }
